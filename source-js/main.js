300de41d3a4877021e9c86ec0af841a0f8b22d82
VolandLarsen <nitro1493@gmail.com>
2017-12-15 23:38:51 +0200

    first commit

---
+++ source-js/main.js
@@ -0,0 +1,65 @@
+function addition(){
+  var num1
+  var num2
+  var result
+  result = num1 + num2
+  return result
+}
+
+function subtraction(){
+  var num1 = 'x1'
+  var num2 = 'x2'
+  var result
+  result = num1 - num2
+  return result
+}
+
+function multiplication(){
+  var num1
+  var num2
+  var result
+  result = num1 * num2
+  return result
+}
+
+function division(){
+  var num1
+  var num2
+  var result
+  if (0 < num2 < 0){
+    result = num1 / num2
+    return result
+  }
+}
+
+function power(){
+  var num1
+  var num2
+  var result
+  result = num1 ** num2
+  return result
+}
+
+function sqrt(){
+  var num1
+  var num2
+  var result
+  if (x1 <= 0){
+    result = num1 ** 1/num2
+    return result
+  }
+}
+
+function sinus(){
+  var num1="x1"
+  var result
+  result = Math.sin(num1)
+  return result
+}
+
+function cos(){
+  var num1="x1"
+  var result
+  result = Math.sin(num1)
+  return result
+}
